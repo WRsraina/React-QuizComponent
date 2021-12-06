9c059a2cd141a3c0c7bee575ea97f0ae385bc410
Sumeet Raina <sraina@worldremit.com>
2021-12-06 21:51:13 +0000

    Displaying prop data in a paragraph tag in the QuizQuestion component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import QuizQuestion from './QuizQuestion'
+import QuizQuestion from './QuizQuestion.js'
 
 let quizData = require('./quiz_data.json')
 
@@ -7,30 +7,17 @@ class Quiz extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      quiz_position: 1,
-      quiz_title: "Title"
+      quiz_position: 1
     }
   }
 
-  handleClick = (event) => {
-      this.setState((state, props) => {
-        return { quiz_position: this.state.quiz_position + 1}
-      })
-
-      console.log(quizData.quiz_questions[this.state.quiz_position-1])
-  }
-
   render() {
-    const question = quizData.quiz_questions[this.state.quiz_position - 1]
-    const {handleClick} = this
-
     return (
-      <div onClick={handleClick}>
-        <QuizQuestion question={question} />
+      <div>
+        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
       </div>
     )
   }
 }
-
 
 export default Quiz
